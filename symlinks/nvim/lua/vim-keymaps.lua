9bc2ac31fc557dee41dc03a7b576e2d66cc45712
Neeraj Singh <neeraj@bigbinary.com>
2026-06-09 18:27:50 -0400

    feat(vim-keymaps.lua): add new key mappings for improved text manipulation and navigation in Neovim
    docs(vim-keymaps.lua): update comments for clarity and add links to demo videos

--- symlinks/nvim/lua/vim-keymaps.lua
+++ symlinks/nvim/lua/vim-keymaps.lua
@@ -11,10 +11,28 @@ vim.keymap.set("x", "p", [["_dP]], { desc = "Paste over selection without losing
 -- Demo at https://youtu.be/XQuNoprFW38?t=500
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "delete without yanking" })
 
--- IN the normal mode Ctrl c will remove all the highlights created by search term
+-- In the normal mode Ctrl c will remove all the highlights created by search term
 -- https://youtu.be/XQuNoprFW38?t=538
 vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlighting", silent = true })
 
+-- In the visual mode use J/K to move selected text
 -- https://youtu.be/XQuNoprFW38?t=551
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+
+-- https://youtu.be/XQuNoprFW38?t=574
+vim.keymap.set("v", "<", "<gv", { desc = "Unindent and keep selection" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent and keep selection" })
+
+-- https://youtu.be/XQuNoprFW38?t=585
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines without moving cursor" })
+
+-- Keep cursor at the center of the page
+-- https://youtu.be/XQuNoprFW38?t=614
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+
+-- https://youtu.be/XQuNoprFW38?t=630
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result cursor centered" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result cursor centered" })
+
