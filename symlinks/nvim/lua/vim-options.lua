7d73a282a12d080dfbbf9696c90a85faab4ad6fa
Neeraj Singh <neeraj@bigbinary.com>
2026-06-09 02:22:22 -0400

    refactor(vim-options.lua): replace vim.cmd with vim.opt for better readability and maintainability of options configuration

--- symlinks/nvim/lua/vim-options.lua
+++ symlinks/nvim/lua/vim-options.lua
@@ -1,8 +1,22 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+local opt = vim.opt
+
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+opt.softtabstop = 2
+
+opt.relativenumber = true
+opt.number = true
+opt.wrap = false
+
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if I include mixed case in my search, assume I want case-sensitive
+
+opt.cursorline = true
+
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert
+
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
