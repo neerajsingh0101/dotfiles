e10fbac746b71aff9ceb24896b5b30bb46cdd089
Neeraj Singh <neeraj@bigbinary.com>
2026-06-11 05:54:00 -0400

    feat(neo-tree.lua): enhance dotfiles handling by ignoring specific files in fs_watch feat(neo-tree.lua): add keymap for closing NeoTree with a description for better usability style(neo-tree.lua): adjust NeoTree window width for improved layout in Neovim

--- symlinks/nvim/lua/plugins/neo-tree.lua
+++ symlinks/nvim/lua/plugins/neo-tree.lua
@@ -9,7 +9,31 @@ return {
     },
     lazy = false, -- neo-tree will lazily load itself
     config = function()
+      local fs_watch = require("neo-tree.sources.filesystem.lib.fs_watch")
+      fs_watch.dotfiles_ignored_names = {
+        ["neo-tree.lua"] = true,
+      }
+
+      if not fs_watch.dotfiles_ignore_wrapped then
+        fs_watch.dotfiles_ignore_wrapped = true
+        local watch_folder = fs_watch.watch_folder
+
+        fs_watch.watch_folder = function(path, callback)
+          return watch_folder(path, function(err, fname)
+            local name = fname and vim.fn.fnamemodify(fname, ":t")
+            if name and fs_watch.dotfiles_ignored_names[name] then
+              return
+            end
+
+            callback(err, fname)
+          end)
+        end
+      end
+
       require("neo-tree").setup({
+        window = {
+          width = 28,
+        },
         filesystem = {
           -- Watch the filesystem so neo-tree refreshes when files are added,
           -- deleted, or renamed outside of Neovim.
@@ -21,7 +45,8 @@ return {
           },
         },
       })
-      vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+      vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {desc = "Reveal NeoTree"})
+      vim.keymap.set("n", "<leader>N", ":Neotree close<CR>", {desc = "Hide NeoTree"})
     end,
   },
 }
