6b801e2142c98b38e0ec2ae22d9d9300b013dcc6
Neeraj Singh <neeraj@bigbinary.com>
2026-06-11 13:01:19 -0400

    feat(plugins): add nvim-surround plugin for easier text surrounding in Neovim fix(telescope): change key mapping for live grep from <leader>lg to <leader>sg for consistency feat(keymaps): add key mappings for window and tab management in Neovim

--- symlinks/nvim/lua/plugins/telescope.lua
+++ symlinks/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,6 @@ return {
     config = function() 
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
     end
 }
--- symlinks/nvim/lua/vim-keymaps.lua
+++ symlinks/nvim/lua/vim-keymaps.lua
@@ -37,3 +37,16 @@ vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result cursor centered" 
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result cursor centered" })
 
 vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>", { desc = "Close quickfix" })
+vim.keymap.set("n", "<leader>lg", "<cmd>terminal lazygit<CR>", { desc = "Open LazyGit" })
+
+-- Window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
